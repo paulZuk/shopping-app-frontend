ca448f0c9e00c843c49f7ca9f2f2610cee65e0a4
paulZuk <k4mpain@gmail.com>
2020-04-17 19:58:49 +0200

    addlist request

---
+++ src/view/addList/actions/AddListActions.ts
@@ -0,0 +1,31 @@
+export enum AddListActions {
+	ADD_LIST = 'ADD_LIST',
+}
+
+export enum Priority {
+	High = 'HIGH',
+	Mid = 'MID',
+	Low = 'LOW',
+}
+
+interface IAddListData {
+	listName: string;
+	priority: Priority;
+	shared?: string | null | undefined;
+}
+
+export interface IAddList {
+	type: typeof AddListActions.ADD_LIST;
+	addListData: IAddListData;
+}
+
+const addList = (addListData: IAddListData): IAddList => {
+	return {
+		type: AddListActions.ADD_LIST,
+		addListData,
+	};
+};
+
+export default {
+	addList,
+};
---
+++ src/view/addList/sagas/AddListSagas.ts
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { call, fork, takeEvery, put } from 'redux-saga/effects';
+import ServerErrorActions from 'core/serverError/actions/ServerErrorActions';
+import { IAddList, AddListActions } from '../actions/AddListActions';
+
+type addListRequestType = (...args: any[]) => any;
+
+const addListRequest: addListRequestType = addListData => {
+	const { listName, priority, shared } = addListData;
+	return axios({
+		method: 'post',
+		url: 'http://localhost:8080/list',
+		data: {
+			listName,
+			priority,
+			shared,
+		},
+	});
+};
+
+export function* addList(action: IAddList) {
+	const data = action.addListData;
+	try {
+		const response = yield call(addListRequest, { ...data });
+
+		if (response.status === 200) {
+		}
+	} catch (err) {
+		const errors = err.response.data.errors;
+
+		if (err.response.status === 422) {
+			yield put(ServerErrorActions.showError(errors));
+		}
+	}
+}
+
+export function* watchAddList() {
+	yield takeEvery(AddListActions.ADD_LIST, addList);
+}
+
+export default function* rootData() {
+	yield fork(watchAddList);
+}
